8a62784a554f1781961fc508eff862edce716400
Val_Sukhinina <vasukhinina@edu.hse.ru>
2024-03-29 11:51:09 +0500

    Добавила автотесты (задание 7)

---
+++ src/tests/collectors/test_news.py
@@ -0,0 +1,36 @@
+"""
+Тестирование функций сбора информации о новостях.
+"""
+import pytest
+
+from collectors.collector import NewsCollector
+from collectors.models import LocationDTO
+
+
+@pytest.mark.asyncio
+class TestNewsCollector:
+    """
+    Тестирование сборщика для получения информации о новостях.
+    """
+
+    location = LocationDTO(
+        capital="Moscow",
+        alpha2code="RU",
+    )
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.collector = NewsCollector()
+
+    async def test_collect_news_success(self):
+        """
+        Тестирование получения информации о новостях.
+        """
+        await self.collector.collect(frozenset([self.location]))
+
+    async def test_read_news_success(self):
+        """
+        Тестирование чтения информации о новостях.
+        """
+        news = await self.collector.read(self.location)
+        assert len(news) == 3
--- src/tests/test_renderer.py
+++ src/tests/test_renderer.py
@@ -1,3 +1,64 @@
 """
 Тестирование функций генерации выходных данных.
 """
+import pytest
+from collectors.models import (
+    CountryDTO,
+    CurrencyInfoDTO,
+    LanguagesInfoDTO,
+    LocationInfoDTO,
+    NewsInfoDTO,
+    WeatherInfoDTO,
+)
+from renderer import Renderer
+
+
+@pytest.mark.asyncio
+class TestRenderer:
+    location = LocationInfoDTO(
+        location=CountryDTO(
+            alpha2code="RU",
+            capital="Moscow",
+            currencies={CurrencyInfoDTO(code="USD")},
+            languages={LanguagesInfoDTO(name="Russian", native_name="Русский")},
+            flag="test",
+            subregion="test",
+            name="Russia",
+            population=3,
+            area=3,
+            longitude=3,
+            latitude=3,
+            alt_spellings=["test"],
+            timezones=[3],
+        ),
+        weather=WeatherInfoDTO(
+            timezone=3,
+            temp=3,
+            pressure=3,
+            humidity=3,
+            wind_speed=3,
+            visibility=3,
+            dt=1,
+            description="test",
+        ),
+        currency_rates={"USD": 1.0},
+        news=[
+            NewsInfoDTO(
+                author="test",
+                title="test",
+                description="test",
+                url="test",
+                publishedAt="test",
+            )
+        ],
+    )
+
+    async def test_format_languages(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_languages()
+        assert result == "Russian (Русский)"
+
+    async def test_format_currencies_rates(self):
+        renderer = Renderer(self.location)
+        result = await renderer._format_currency_rates()
+        assert result == "USD = 1.00 руб."
